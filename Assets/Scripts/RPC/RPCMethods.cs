b81b7929566f3f8a5424af39a7c3a313e171308a
neverdieone <altiore@mail.ru>
2024-02-11 13:17:30 +0300

    Add used methods

--- Assets/Scripts/RPC/RPCMethods.cs
+++ Assets/Scripts/RPC/RPCMethods.cs
@@ -1,22 +1,48 @@
+using System.Collections.Generic;
 using AustinHarris.JsonRpc;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class RpcMethods : JsonRpcService {
-    public Player GetPlayer() {
+
+    private Player GetPlayer() {
         return GameObject.Find("Player").GetComponent<Player>();
     }
 
     [JsonRpcMethod]
-    void ExampleMethod() {
-        Debug.Log("Example Method");
-        Player player = GetPlayer();
-        // player.DoSomething();
+    void LoadScene(string sceneName) {
+        Debug.Log("Load scene: " + sceneName);
+        SceneManager.LoadScene(sceneName);
     }
 
     [JsonRpcMethod]
-    int Some(int i) {
-        Debug.Log(i);
-        return i;
+    Dictionary<string, float> GetObjectCoordinates(string objectName) {
+        Debug.Log("Get coords: " + objectName);
+        GameObject gameObject = GameObject.Find(objectName);
+        Vector3 gameObjectPosition = gameObject.transform.position;
+        return new Dictionary<string, float> {
+            {"x", gameObjectPosition.x},
+            {"y", gameObjectPosition.y},
+        };
+    }
+
+    [JsonRpcMethod]
+    void TeleportPlayer(float x, float y) {
+        Player _player = GetPlayer();
+        Vector3 newPosition = new(x, y);
+        _player.transform.position = newPosition;
+
+    }
+
+    [JsonRpcMethod]
+    bool IsObjectPresent(string objectName) {
+        GameObject gameObject = GameObject.Find(objectName);
+        return gameObject;
+    }
+
+    [JsonRpcMethod]
+    int GetItemCount(string itemName) {
+        return Managers.Inventory.GetItemCount(itemName);
     }
 }
